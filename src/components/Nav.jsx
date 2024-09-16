16bdbdb8a80ecb5ae85e0d6ccd0a1213888365d5
Joanna Rahhal <joanna.h.rahhal@gmail.com>
2024-09-16 18:11:13 +0300

    Altered navigation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,44 +9,50 @@ const Nav = () => {
     setToggle(!toggle);
   };
   return (
-    <div className="p-5 ">
-      <div className="sm:hidden md:flex md:items-center md:justify-between ">
-        <p className="text-lg font-bold lg:text-2xl">OG/</p>
-        <ul className="h-full w-3/4 flex items-center justify-between bg-white">
-          <a href="#home">
+    <div className="p-4 md:p-8 ">
+      <div className="hidden md:flex md:items-center md:justify-between ">
+        <ul className="h-full w-3/4 flex items-center justify-start gap-24 bg-white">
+          <a href="#home" className="cursor-pointer">
             <li>Home</li>
           </a>
-          <a href="About">
+          <a href="collection" className="cursor-pointer">
+            <li>Collection</li>
+          </a>
+        </ul>
+        <p className="font-bold lg:text-xl">Orion Gallery</p>
+        <ul className="h-full w-3/4 flex items-center justify-end gap-24 bg-white">
+          <a href="#event" className="cursor-pointer">
+            <li>Event</li>
+          </a>
+          <a href="#about" className="cursor-pointer">
             <li>About Us</li>
           </a>
-          <li>
-            CONTACT@ORIONGALLERY.COM
-            <p>(+555) 867-4321</p>
-          </li>
         </ul>
       </div>
-      <div className="text-sm flex justify-between items-center relative md:hidden">
-        <p className="text-sm font-bold">OG/</p>
+      <div className="text-sm flex justify-between items-center gap-4 relative md:hidden">
+        <p className="text-sm font-bold">Orion Gallery</p>
         <RxHamburgerMenu onClick={handleToggle} size={18} />
         {toggle && (
-          <div className="h-screen w-full fixed top-0 left-0 z-40">
+          <div className="h-screen w-full fixed top-0 left-0 z-40 bg-white flex items-center justify-center">
             <IoCloseOutline
-              className="absolute right-3 top-5"
+              className="absolute right-3 top-5 cursor-pointer"
               size={24}
               onClick={handleToggle}
             />
 
-            <ul className="h-full flex flex-col items-center justify-evenly bg-white">
-              <a href="">
+            <ul className="h-full w-3/4 flex flex-col items-center justify-center gap-24 bg-white">
+              <a href="#home" className="cursor-pointer">
                 <li>Home</li>
               </a>
-              <a href="">
+              <a href="collection" className="cursor-pointer">
+                <li>Collection</li>
+              </a>
+              <a href="#event" className="cursor-pointer">
+                <li>Event</li>
+              </a>
+              <a href="#about" className="cursor-pointer">
                 <li>About Us</li>
               </a>
-              <li>
-                CONTACT@ORIONGALLERY.COM
-                <p>(+555) 867-4321</p>
-              </li>
             </ul>
           </div>
         )}
